d63c24134ee1eb482373e46725a5cb79dd5eefae
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-07-30 15:05:28 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-var fs = require('fs');
-var files = fs.readdirSync('/IG pg/img');
+var data = {
+    "images": [{
+            "img": "img/balloon.jpg"
+        },
+        {
+            "img": "img/cate.jpg"
+        },
+        {
+            "img": "img/dragon.jpg"
+        },
+        {
+            "img": "img/Groot.jpg"
+        },
+        {
+            "img": "img/flareon.jpg"
+        },
+        {
+            "img": "img/forest.jpg"
+        },
+        {
+            "img": "img/fox.jpg"
+        },
+        {
+            "img": "img/glaceon.jpg"
+        },
+        {
+            "img": "img/majora.jpg"
+        },
+
+
+    ]
+};
+data.images.forEach(function(obj) {
+    var img = new Image();
+    img.src = obj.img;
+    img.setAttribute("class", "art-gallery");
+    document.getElementById("img-container").appendChild(img);
+
+});
